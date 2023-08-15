48a85510ba82d78e140eae624684f4c3a492ab0b
qianmunan <fanjunran@outlook.com>
2023-08-15 16:32:04 +0800

    test topic server

---
+++ topic_ros/src/topic_test_cpp/src/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char ** argv)
+{
+    rclcpp::init(argc,argv);
+
+    auto node = rclcpp::Node::make_shared("helloworld_node");
+
+    RCLCPP_INFO(node->get_logger(),"hello ROS2 Humble!!");
+
+    rclcpp::shutdown();
+
+    return 0;
+
+}
