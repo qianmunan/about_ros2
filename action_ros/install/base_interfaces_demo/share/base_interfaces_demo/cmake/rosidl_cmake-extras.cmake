c1d8b6f0872280e48cd7c81d9fbd3d4bb80fc25e
qianmunan <fanjunran@outlook.com>
2023-08-17 22:35:54 +0800

    ros action create

---
+++ action_ros/install/base_interfaces_demo/share/base_interfaces_demo/cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(base_interfaces_demo_IDL_FILES "action/Progress.idl")
+set(base_interfaces_demo_INTERFACE_FILES "action/Progress.action")
