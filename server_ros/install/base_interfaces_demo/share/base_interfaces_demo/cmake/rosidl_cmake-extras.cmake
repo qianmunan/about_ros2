3825cc88ba81abe58a49175c956153d4f960bb64
qianmunan <fanjunran@outlook.com>
2023-08-18 06:13:13 +0800

    ros service create

---
+++ server_ros/install/base_interfaces_demo/share/base_interfaces_demo/cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(base_interfaces_demo_IDL_FILES "srv/AddInts.idl")
+set(base_interfaces_demo_INTERFACE_FILES "srv/AddInts.srv;srv/AddInts_Request.msg;srv/AddInts_Response.msg")
---
+++ server_ros/src/first_hello_test/src/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char ** argv)
+{
+    rclcpp::init(argc, argv);
+
+    auto node = rclcpp::Node::make_shared("helloworld_node");
+
+    RCLCPP_INFO(node->get_logger(), "hello world server ros");
+
+    rclcpp::shutdown();
+
+  return 0;
+}
